56e8b429b9e5b95ea34518fb3632f0f3431790fd
naira0501 <nairaaghayan19@gmail.com>
2024-05-05 15:54:03 +0400

    initial commit

---
+++ file.js
@@ -0,0 +1,10 @@
+ const showSidebar =  () => {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display='flex';
+}
+
+const hideSidebar = () => {
+    const sidebar = document.querySelector('.side-bar');
+    sidebar.style.display='none';
+}
+

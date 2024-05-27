bb297b45c630c74071380cf9ca6390799cc6649c
Naira <nairaaghayan19@gmail.com>
2024-05-27 14:07:50 +0400

    added side bar animation

--- file.js
+++ file.js
@@ -1,10 +1,15 @@
- const showSidebar =  () => {
-    const sidebar = document.querySelector('.side-bar');
-    sidebar.style.display='flex';
-}
+const showSidebar = () => {
+  const sidebar = document.querySelector(".side-bar-wrapper");
+  const humburgerIcon = document.querySelector(".hamburger-menu");
+
+  humburgerIcon.style.visibility = "hidden";
+  sidebar.classList.add("side-bar-wrapper_visible");
+};
 
 const hideSidebar = () => {
-    const sidebar = document.querySelector('.side-bar');
-    sidebar.style.display='none';
-}
+  const sidebar = document.querySelector(".side-bar-wrapper");
+  const humburgerIcon = document.querySelector(".hamburger-menu");
 
+  humburgerIcon.style.visibility = "visible";
+  sidebar.classList.remove("side-bar-wrapper_visible");
+};
